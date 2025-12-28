373e1401fdb02e694785043c10670f57ae6e4494
Canhdangranh <133893026+Canhdangranh@users.noreply.github.com>
2025-12-28 22:17:17 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // THÊM DÒNG NÀY: Thay tên repo của bạn vào giữa 2 dấu gạch chéo
+      base: '/SmartPrintSpooler/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
